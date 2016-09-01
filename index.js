a27b680ba2f59202bdab1da6b9bdccca76a80bc7
Thien. Lam Phuoc <Thien. Lam Phuoc>
2016-09-01 16:53:33 +0700

    add user is typing

--- index.js
+++ index.js
@@ -18,9 +18,46 @@ const server = app.listen(3000,function(){
 });
 var io = require("socket.io")(server);
 
+var users=[];
 io.on('connection', function(socket){
-  console.log('hello: ');
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+
+  var username='';
+
+  socket.emit('request-user',{message: 'Please enter username: '});
+
+  socket.on('add-user',function(data){
+    if(users.indexOf(data.username) == -1){
+      username = data.username;
+      users.push(data.username);
+      console.log(username+' has connected!');
+      io.emit('sayhello',{username: data.username});
+    }
+    else {
+      socket.emit('request-user',{message: 'Username Already Exists\nPlease enter different username: '});
+    }
   });
+
+  socket.on('is-typing', function (data) {
+    socket.broadcast.emit('typing', {
+      username: data.username
+    });
+  });
+
+  socket.on('stop-typing', function (data) {
+    socket.broadcast.emit('stop-typing', {
+      username: data.username
+    });
+  });
+
+  socket.on('send-message', function(data){
+    io.emit('send-message', {message: data.message,username: data.username});
+  });
+
+  socket.on('disconnect',function(){
+    console.log(username+' has disconnected!');
+    socket.broadcast.emit('stop-typing', {username: username});
+    users.splice(users.indexOf(username),1);
+    io.emit('remove-user',{message: ' has left!',username:username});
+  })
+
 });
